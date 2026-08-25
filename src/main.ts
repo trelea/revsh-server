7d8baf392ff8d06bbbad8f64cc8481c5971b33ec
Trelea Marius <treleamarius76@gmail.com>
2026-08-25 16:28:01 +0300

    0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
